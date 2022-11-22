5e8479c4d074a195d847424920f6cb332ba865cd
Anob Takon <anobtakon@gmail.com>
2022-11-22 00:59:51 -1100

    learn

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let sex = document.querySelector(".value").value;
+let sex = ;
 document.querySelector(".btn").addEventListener("click", function () {
-  console.log(sex);
+  console.log(document.querySelector(".value").value);
 });
 // Add multiplication and division to the useReducer and modulus
