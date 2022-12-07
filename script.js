f72328c2fcfeb2394de02902f4dee7636b275ae7
Anob Takon <anobtakon@gmail.com>
2022-12-07 23:51:15 +0000

    javascript

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 document.querySelector(".btn").addEventListener("click", function () {
   console.log(document.querySelector(".value").value);
 });
+
+document;
 // Add multiplication and division to the useReducer and modulus
